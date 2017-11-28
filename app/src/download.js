7552771e69d50cf16ff74be9f3ea26d286ec3266
Christoph Wiechert <wio@psitrax.de>
2017-11-28 16:37:26 +0100

    Fix: IE11 has no String.startsWith

--- app/src/download.js
+++ app/src/download.js
@@ -1,3 +1,10 @@
+if(!String.prototype.startsWith) {
+  String.prototype.startsWith = function(searchString, position) {
+    position = position || 0;
+    return this.indexOf(searchString, position) === position;
+  };
+}
+
 import Vue from 'vue';
 import Download from './Download.vue';
 import Icon from 'vue-awesome/components/Icon.vue'
--- app/src/upload.js
+++ app/src/upload.js
@@ -1,3 +1,10 @@
+if(!String.prototype.startsWith) {
+  String.prototype.startsWith = function(searchString, position) {
+    position = position || 0;
+    return this.indexOf(searchString, position) === position;
+  };
+}
+
 import Vue from 'vue';
 import Upload from './Upload.vue';
 import store from './Upload/store.js';
