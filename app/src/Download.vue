10493b925fc7bf8f5832dcb449037c13ad2997ce
Dekleor <donier.matteo@gmail.com>
2022-10-21 14:42:28 +0200

    wreck it matt

--- app/src/Download.vue
+++ app/src/Download.vue
@@ -21,16 +21,6 @@
       .panel-heading
         strong {{ $root.lang.files }}
           div.h3 {{ $root.lang.ongoingDownload }}
-    .panel.panel-primary(v-else='!needspassword && !loading')
-      .panel-heading
-        strong {{ $root.lang.files }}
-          div.pull-right.btn-group.btn-download-archive
-            a.btn.btn-sm.btn-default(@click="downloadAll('zip')", :title="$root.lang.zipDownload")
-              icon.fa-fw(name="download")
-              |  zip
-            a.btn.btn-sm.btn-default(@click="downloadAll('tar.gz')", :title="$root.lang.tarGzDownload")
-              icon.fa-fw(name="download")
-              |  tar.gz
       .panel-body
         table.table.table-hover.table-striped.files
           tbody
