dcc30b9fad3e8ec51e9aecdc4b75e832a4140701
Dekleor <donier.matteo@gmail.com>
2022-10-21 15:56:25 +0200

    testalacon

--- app/src/Download.vue
+++ app/src/Download.vue
@@ -14,17 +14,14 @@
       p.text-danger(v-show='passwordWrong')
         strong {{ $root.lang.accessDenied }}
       |
-      button.decrypt.btn.btn-primary(:disabled='password.length<1', @click='fetchBucket()' )
-      icon.fa-fw(name="key" )
-      | {{ $root.lang.decrypt }}
+      button.decrypt.btn.btn-primary(:disabled='password.length<1', @click='fetchBucket()')
+        icon.fa-fw(name="key")
+        |  {{ $root.lang.decrypt }}
     .panel.panel-primary
       .panel-heading
         strong {{ $root.lang.files }}
-          <?php
-            if (loading)
-            {
-            echo $root.lang.ongoingDownload;
-            } ?>
+          .row.overall-process(v-show="state === 'uploading'")
+            span.h3(v-show='percentUploaded<100') {{ $root.lang.ongoingDownload }}
 </template>
 
 
