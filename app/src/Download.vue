e98f0f9719ed42cd6229f7dbfa1f028f964fcc3c
Dekleor <donier.matteo@gmail.com>
2022-10-21 15:11:51 +0200

    More baby steps

--- app/src/Download.vue
+++ app/src/Download.vue
@@ -21,27 +21,6 @@
       .panel-heading
         strong {{ $root.lang.files }}
           h3 {{ $root.lang.ongoingDownload }}
-      .panel-body
-        table.table.table-hover.table-striped.files
-          tbody
-            tr(v-for='file in files', style='cursor: pointer', @click='download(file)')
-              td.file-icon
-                file-icon(:file='file')
-              td
-                div.pull-right.btn-group
-                  clipboard.btn.btn-sm.btn-default(:value='baseURI + file.url', @change='copied(file, $event)', :title='$root.lang.copyToClipboard')
-                    a
-                      icon(name="copy")
-                  a.btn.btn-sm.btn-default(:title="$root.lang.preview", @click.prevent.stop="preview=file", v-if="file.previewType")
-                    icon(name="eye")
-                i.pull-right.fa.fa-check.text-success.downloaded(v-show='file.downloaded')
-                p
-                  strong {{ file.metadata.name }}
-                  small.file-size(v-if="isFinite(file.size)") ({{ humanFileSize(file.size) }})
-                p {{ file.metadata.comment }}
-
-    preview-modal(:preview="preview", :files="previewFiles", :max-size="config.maxPreviewSize", @close="preview=false")
-
 </template>
 
 
