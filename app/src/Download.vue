02c08a6804d33f2361067c185fa38c51e2f016bf
Dekleor <donier.matteo@gmail.com>
2022-10-21 16:01:12 +0200

    tatonnement again

--- app/src/Download.vue
+++ app/src/Download.vue
@@ -20,7 +20,7 @@
     .panel.panel-primary
       .panel-heading
         strong {{ $root.lang.files }}
-          .row.overall-process(v-show="state === 'uploading'")
+          .row.overall-process(v-show="state === 'loading'")
             span.h3(v-show='percentUploaded<100') {{ $root.lang.ongoingDownload }}
 </template>
 
